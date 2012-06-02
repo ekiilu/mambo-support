3eadec3a912fc5fa70db31ff1b8e2f7952173152
Paul Schuegraf <paul@verticallabs.ca>
2012-06-01 17:36:05 -0700

    default values for params

--- lib/support/params_helper.rb
+++ lib/support/params_helper.rb
@@ -1,21 +1,23 @@
 # -*- encoding : utf-8 -*-
 module Support::ParamsHelper
-  def session_param(name, context)
-    handle_session_param(name, [name], [context, name])
+  def session_param(name, context, default = nil)
+    handle_session_param(name, [name], [context, name], default)
   end
 
-  def sort_param(context)
-    handle_session_param(:sort_key, [:sort_key], [context, :sort_key])
-    handle_session_param(:sort_order, [:sort_order], [context, :sort_order])
+  def sort_param(context, defaults)
+    raise if defaults.keys.length > 1
+    handle_session_param(:sort_key, [:sort_key], [context, :sort_key], defaults.keys.first)
+    handle_session_param(:sort_order, [:sort_order], [context, :sort_order], defaults.values.first)
   end
 
-  def filter_param(context)
-    handle_session_param(:filter_key, [:filter_key], [context, :filter_key])
-    handle_session_param(:filter_value, [:filter_value], [context, :filter_value])
+  def filter_param(context, defaults)
+    raise if defaults.keys.length > 1
+    handle_session_param(:filter_key, [:filter_key], [context, :filter_key], defaults.keys.first)
+    handle_session_param(:filter_value, [:filter_value], [context, :filter_value], defaults.values.first)
   end
 
   def valid_sort
-    (@sort_key && @sort_order) ? @sort_key.to_sym.send(@sort_order.to_sym) : {}
+    (@sort_key && @sort_order) ? { :order => @sort_key.to_sym.send(@sort_order.to_sym)} : {}
   end
 
   def valid_filter
@@ -23,9 +25,9 @@ module Support::ParamsHelper
   end
 
 private
-  def handle_session_param(var_name, param_keys, session_keys)
+  def handle_session_param(var_name, param_keys, session_keys, default = nil)
     val = traverse_hash(params, param_keys) || traverse_hash(session, session_keys)
-    val = nil if val.blank?
+    val = default if val.blank?
     instance_variable_set("@#{var_name}".to_sym, val)
     traverse_hash(session, session_keys, val)
   end
