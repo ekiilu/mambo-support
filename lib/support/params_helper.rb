ef5c002552a0058c083fe3932f83edd0003fd6af
Chris Dion <chris@verticallabs.ca>
2012-06-03 18:43:08 -0700

    sorting

--- lib/support/params_helper.rb
+++ lib/support/params_helper.rb
@@ -1,46 +1,37 @@
 # -*- encoding : utf-8 -*-
 module Support::ParamsHelper
-  def session_param(name, context, default = nil)
-    handle_session_param(name, [name], [context, name], default)
-  end
+	#
+	def session_param(context, key, default = nil)
+		handle_session_value(context, key, default)
+	end
 
-  def sort_param(context, defaults = {})
-    raise if defaults.keys.length > 1
-    handle_session_param(:sort_key, [:sort_key], [context, :sort_key], defaults.keys.first)
-    handle_session_param(:sort_order, [:sort_order], [context, :sort_order], defaults.values.first)
-  end
+	#
+	def page_param(context)
+		handle_session_value(context, :page, 1)
+	end
 
-  def filter_param(context, defaults = {})
-    raise if defaults.keys.length > 1
-    handle_session_param(:filter_key, [:filter_key], [context, :filter_key], defaults.keys.first)
-    handle_session_param(:filter_value, [:filter_value], [context, :filter_value], defaults.values.first)
-  end
+	#
+	def sort_param(context, key, order)
+		handle_session_symbol(context, :sort_key, key)
+		handle_session_symbol(context, :sort_order, order)
+	end
 
-  def valid_sort
-    (@sort_key && @sort_order) ? {:order => [@sort_key.to_sym.send(@sort_order.to_sym)]} : {}
-  end
+	#
+	def handle_session_value(context, key, default)
+		handle_session_param(context, key, default)
+	end
 
-  def valid_filter
-    (@filter_key && @filter_value) ? {@filter_key.to_sym => @filter_value.to_sym} : {}
-  end
+	#
+	def handle_session_symbol(context, key, default)
+		handle_session_param(context, key, default) { |value| value.to_sym }
+	end
 
-private
-  def handle_session_param(var_name, param_keys, session_keys, default = nil)
-    val = traverse_hash(params, param_keys) || traverse_hash(session, session_keys)
-    val = default if val.blank?
-    instance_variable_set("@#{var_name}".to_sym, val)
-    traverse_hash(session, session_keys, session_keys.last.to_sym => val)
-  end
-
-  def traverse_hash(hash, keys, merge = {})
-    keys.inject(hash) do |s, k|
-      if k == keys.last
-        merge.each_pair {|k, v| s[k] = v}
-        return s[k]
-      else
-        s[k] ||= {}
-        s = s[k]
-      end
-    end
-  end
+	#
+	def handle_session_param(context, key, default, &block)
+		session[context] ||= {}
+		value = params[key] || session[context][key] || default
+		value = yield(value) if block_given?
+		instance_variable_set("@#{key}".to_sym, value)
+		session[context][key] = value
+	end
 end
