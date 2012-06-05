9ab7043390e793e71a553d0fcf61fbe95e8b7435
Chris Dion <chris@verticallabs.ca>
2012-06-05 13:20:51 -0700

    filtering

--- lib/support/params_helper.rb
+++ lib/support/params_helper.rb
@@ -2,41 +2,31 @@
 module Support::ParamsHelper
 	#
 	def session_param(context, key, default = nil)
-		handle_session_value(context, key, default)
-	end
-
-	#
-	def page_param(context)
-		handle_session_value(context, :page, 1)
-	end
-
-	#
-	def sort_param(context, key, order)
-		handle_session_symbol(context, :sort_key, key)
-		handle_session_symbol(context, :sort_order, order)
-	end
-
-	#
-	def filter_param(context)
-		handle_session_symbol(context, :filter_key, "")
-		handle_session_value(context, :filter_value, "")
-	end
-
-	#
-	def handle_session_value(context, key, default)
 		handle_session_param(context, key, default)
 	end
 
 	#
-	def handle_session_symbol(context, key, default)
-		handle_session_param(context, key, default) { |value| value.to_sym if value}
+	def page_param(context)
+		handle_session_param(context, :page, 1)
 	end
 
 	#
-	def handle_session_param(context, key, default, &block)
+	def sort_param(context, key, order)
+		handle_session_param(context, :sort_key, key)
+		handle_session_param(context, :sort_order, order)
+	end
+
+	#
+	def filter_param(context)
+		handle_session_param(context, :filter_key, :none)
+		handle_session_param(context, :filter_value, "")
+	end
+
+	#
+	def handle_session_param(context, key, default)
 		session[context] ||= {}
 		value = params[key] || session[context][key] || default
-		value = yield(value) if block_given?
+		value = value.to_sym if !value.nil? && default.is_a?(Symbol)
 		instance_variable_set("@#{key}".to_sym, value)
 		session[context][key] = value
 	end
--- lib/support/table_helper.rb
+++ lib/support/table_helper.rb
@@ -17,13 +17,15 @@ module Support::TableHelper
     content = capture { yield }.strip
     active = @filter_key && @filter_value && key == @filter_key
 
+    options.unshift(["- All -", ""])
+
     content_tag(:th) do
       raw(content) +
       content_tag(:div, :class => "filter #{(active ? 'active' : nil)}") do
         form_tag("", :method => :get) do
           hidden_field_tag(:page, 1) +
           hidden_field_tag(:filter_key, key) +
-          select_tag(:filter_value, options_for_select(options, @filter_value), :include_blank => true, :onchange => submit_form)
+          select_tag(:filter_value, options_for_select(options, @filter_value), :onchange => submit_form)
         end
       end +
 			content_tag(:div, :class => "sort") do
