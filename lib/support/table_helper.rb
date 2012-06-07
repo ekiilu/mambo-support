a6638ed47a0e60eff65bb9724b369e3cfbb71412
Paul Schuegraf <paul@verticallabs.ca>
2012-06-07 15:43:28 -0700

    table helper

--- lib/support/table_helper.rb
+++ lib/support/table_helper.rb
@@ -4,11 +4,11 @@ module Support::TableHelper
   def sorted_table_header_tag(key, &block)
     content = capture { yield }.strip
     content_tag(:th) do
-      raw(content) +
-      content_tag(:div, :class => "sort") do
-        link_to("+", params.merge(:page => 1, :sort_key => key, :sort_order => :asc), :class => "ascending") +
-        link_to("-", params.merge(:page => 1, :sort_key => key, :sort_order => :desc), :class => "descending")
-      end
+      content_tag(:div, content, :class => 'title') +
+			content_tag(:div, :class => "sort") do
+				link_to("+", params.merge(:page => 1, :sort_key => key, :sort_order => :asc), :class => "ascending #{active_class(@sort_key == key && @sort_order == :asc)}") +
+				link_to("-", params.merge(:page => 1, :sort_key => key, :sort_order => :desc), :class => "descending #{active_class(@sort_key == key && @sort_order == :desc)}")
+			end
     end
   end
 
@@ -20,18 +20,20 @@ module Support::TableHelper
     options.unshift(["- All -", ""])
 
     content_tag(:th) do
-      raw(content) +
+      content_tag(:div, content, :class => 'title') +
       content_tag(:div, :class => "filter #{(active ? 'active' : nil)}") do
         form_tag("", :method => :get) do
           hidden_field_tag(:page, 1) +
           hidden_field_tag(:filter_key, key) +
           select_tag(:filter_value, options_for_select(options, @filter_value), :onchange => submit_form)
         end
-      end +
-			content_tag(:div, :class => "sort") do
-				link_to("+", params.merge(:page => 1, :sort_key => key, :sort_order => :asc), :class => "ascending") +
-				link_to("-", params.merge(:page => 1, :sort_key => key, :sort_order => :desc), :class => "descending")
-			end
+      end
+    end
+  end
+
+  def table_header_tag(title)
+    content_tag(:th) do
+      content_tag(:div, title, :class => 'title')
     end
   end
 end
