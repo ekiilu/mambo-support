6b4bc3de8197c32bad0264e3d41df8280916b44a
Chris Dion <chris@verticallabs.ca>
2012-10-10 20:17:48 -0700

    Update version to 0.0.11

--- lib/support/engine.rb
+++ lib/support/engine.rb
@@ -1,11 +1,7 @@
+#-  -*- encoding : utf-8 -*- 
 #- This Source Code Form is subject to the terms of the Mozilla Public
 #- License, v. 2.0. If a copy of the MPL was not distributed with this
 #- file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-require 'rails'
-require 'support/table_helper'
-require 'support/params_helper'
-require 'support/form_helper'
 
 module Support
   class Engine < Rails::Engine
@@ -16,6 +12,8 @@ module Support
       ActionView::Base.send(:include, Support::FormHelper)
 
       ActionController::Base.send(:include, Support::ParamsHelper)
+
+      ActiveRecord::Base.send(:include, Support::ModelHelper)
     end
   end
 end
--- lib/support/params_helper.rb
+++ lib/support/params_helper.rb
@@ -1,3 +1,4 @@
+#-  -*- encoding : utf-8 -*- 
 #- This Source Code Form is subject to the terms of the Mozilla Public
 #- License, v. 2.0. If a copy of the MPL was not distributed with this
 #- file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -32,6 +33,7 @@ private
 	def handle_session_param(context, key, default)
 		session[context] ||= {}
 		value = params.include?(key) ? params[key] : session[context][key] || default
+		value = value.to_s if !value.nil?
 		instance_variable_set("@#{key}".to_sym, value)
 		session[context][key] = value
 	end
